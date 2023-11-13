e659979c290f1baa18e604c5c78c41db06b18581
JimAtLang <jim.stewart@thelangschool.org>
2023-11-13 14:32:37 +0000

    Initial commit

---
+++ Desktop.java
@@ -0,0 +1,18 @@
+class Desktop{
+  String manufacturer;
+  String os;
+  int ram;
+  int storage;
+  String chipType;
+  int monitors;
+  boolean wired;
+  public Desktop(String manufacturer, String os, int ram, int storage, int monitors, boolean wired){
+    this.manufacturer = manufacturer;
+    this.os = os;
+    this.ram = ram;
+    this.storage = storage;
+    this.monitors = monitors;
+    this.wired = wired;
+  }
+}
+
---
+++ Laptop.java
@@ -0,0 +1,17 @@
+class Laptop{
+  String manufacturer;
+  String os;
+  int ram;
+  int storage;
+  string chipType;
+  int weight;
+  boolean touchscreen;
+  public Laptop(String manufacturer, String os, int ram, int storage, int weight, boolean touchscreen){
+    this.manufacturer = manufacturer;
+    this.os = os;
+    this.ram = ram;
+    this.storage = storage;
+    this.weight = weight;
+    this.touchscreen = touchscreen;
+  }
+}
